1df62f9f92d669ff7884d9c06d7ecc1132c70f49
Jaredd <jaredd@Jared-Dahlkes-Macbook.local>
2023-02-26 17:20:48 -0800

    await app prepare

--- server/index.js
+++ server/index.js
@@ -17,78 +17,79 @@ const baseUrl = process.env.BASE_URL || 'http://localhost:3000'
 
 ;(async () => {
 	try {
-		await app.prepare()
-		const server = express()
+		await app.prepare().then(() => {
+			const server = express()
 
-		server.use(
-			session({
-				secret: process.env.SESSION_SECRET || 'secret',
-				store: MongoStore.create({
-					mongoUrl: dev
-						? process.env.NEXT_PUBLIC_MONGODB_URI_DEV
-						: process.env.NEXT_PUBLIC_MONGODB_URI
-				}),
-				unset: 'destroy',
-				resave: true,
-				saveUninitialized: true
+			server.use(
+				session({
+					secret: process.env.SESSION_SECRET || 'secret',
+					store: MongoStore.create({
+						mongoUrl: dev
+							? process.env.NEXT_PUBLIC_MONGODB_URI_DEV
+							: process.env.NEXT_PUBLIC_MONGODB_URI
+					}),
+					unset: 'destroy',
+					resave: true,
+					saveUninitialized: true
+				})
+			)
+			server.use(passport.initialize())
+			server.use(passport.session())
+			const map = {
+				user: new Map()
+			}
+			passport.serializeUser(function (user, done) {
+				done(null, user.id)
 			})
-		)
-		server.use(passport.initialize())
-		server.use(passport.session())
-		const map = {
-			user: new Map()
-		}
-		passport.serializeUser(function (user, done) {
-			done(null, user.id)
-		})
-		passport.deserializeUser(function (id, done) {
-			done(null, map.user.get(id) || null)
-		})
-		passport.use(
-			new LnurlAuth.Strategy(function (linkingPublicKey, done) {
-				let user = map.user.get(linkingPublicKey)
-				if (!user) {
-					user = { id: linkingPublicKey }
-					map.user.set(linkingPublicKey, user)
-				}
-				done(null, user)
+			passport.deserializeUser(function (id, done) {
+				done(null, map.user.get(id) || null)
 			})
-		)
-		server.use(passport.authenticate('lnurl-auth'))
+			passport.use(
+				new LnurlAuth.Strategy(function (linkingPublicKey, done) {
+					let user = map.user.get(linkingPublicKey)
+					if (!user) {
+						user = { id: linkingPublicKey }
+						map.user.set(linkingPublicKey, user)
+					}
+					done(null, user)
+				})
+			)
+			server.use(passport.authenticate('lnurl-auth'))
 
-		server.get(
-			'/login',
-			function (req, res, next) {
+			server.get(
+				'/login',
+				function (req, res, next) {
+					if (req.user) {
+						return res.redirect(baseUrl + '/home')
+					}
+					next()
+				},
+				new LnurlAuth.Middleware({
+					callbackUrl: baseUrl + '/login',
+					cancelUrl: baseUrl,
+					loginTemplateFilePath: 'server/login.html'
+				})
+			)
+			server.get('/user', (req, res) => {
+				res.send(req.user)
+			})
+
+			server.get('/logout', function (req, res, next) {
 				if (req.user) {
-					return res.redirect(baseUrl + '/home')
+					req.session.destroy()
+					return res.redirect(baseUrl)
 				}
 				next()
-			},
-			new LnurlAuth.Middleware({
-				callbackUrl: baseUrl + '/login',
-				cancelUrl: baseUrl,
-				loginTemplateFilePath: 'server/login.html'
 			})
-		)
-		server.get('/user', (req, res) => {
-			res.send(req.user)
-		})
 
-		server.get('/logout', function (req, res, next) {
-			if (req.user) {
-				req.session.destroy()
-				return res.redirect(baseUrl)
-			}
-			next()
-		})
+			server.all('*', (req, res) => {
+				return handle(req, res)
+			})
 
-		server.all('*', (req, res) => {
-			return handle(req, res)
-		})
-
-		server.listen(port, (err) => {
-			if (err) throw err
-			console.log(`Ready on ${baseUrl}`)
+			server.listen(port, (err) => {
+				if (err) throw err
+				console.log(`Ready on ${baseUrl}`)
+			})
 		})
 	} catch (e) {
 		console.error(e)
