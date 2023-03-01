04a5471c1f65718078e0e612e873ad82595b66a2
Jaredd <jaredd@Jared-Dahlkes-Macbook.local>
2023-03-01 11:09:59 -0800

    updates

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,28 +1,11 @@
-import { useTranslation } from 'next-i18next'
-import EmailSignIn from 'pages/auth/signin/email'
 import LnurlAuthSignIn from 'pages/auth/signin/lnurl'
-import PhoneSignIn from 'pages/auth/signin/phone'
-import React, { useState } from 'react'
-import { LoginMethod, loginMethods } from 'types/LoginMethod'
+import React from 'react'
 
 type LoginProps = {
-	instructionsText?(loginMethod: LoginMethod): string
-	submitText?: string
 	callbackUrl?: string
-	tipId?: string
-	defaultLoginMethod: LoginMethod
-	allowedLoginMethods?: LoginMethod[]
 	isPreview?: boolean
 }
 
-export function Login({
-	submitText,
-	callbackUrl,
-	instructionsText,
-	tipId,
-	defaultLoginMethod,
-	allowedLoginMethods,
-	isPreview
-}: LoginProps) {
+export function Login({ callbackUrl, isPreview }: LoginProps) {
 	return <LnurlAuthSignIn callbackUrl={callbackUrl} isPreview={isPreview} />
 }
