a56520f71fb3dfe3fbaa09c09fdd0b5680f05d24
Jaredd <jaredd@Jared-Dahlkes-Macbook.local>
2023-03-01 17:22:54 -0800

    simplify styling

--- components/LightningQRCode.tsx
+++ components/LightningQRCode.tsx
@@ -4,15 +4,5 @@ import QRCode, { QRCodeProps } from 'react-qr-code'
 type LightningQRCodeProps = QRCodeProps
 
 export function LightningQRCode(props: LightningQRCodeProps) {
-	return (
-		<QRCode
-			{...(props as any)}
-			size={256}
-			style={{
-				height: 'auto',
-				maxWidth: '100%',
-				width: '100%'
-			}}
-			viewBox={`0 0 256 256`}></QRCode>
-	)
+	return <QRCode {...(props as any)}></QRCode>
 }
