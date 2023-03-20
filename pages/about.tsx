93238fa33f1eff4669c8b484f7d0b5841523441c
Jaredd <jaredd@Jared-Dahlkes-Macbook.local>
2023-03-19 20:45:01 -0700

    adds total messages stat

--- pages/about.tsx
+++ pages/about.tsx
@@ -105,7 +105,13 @@ export default function About({
 			refetchInterval: 8000
 		}
 	)
-	console.log('users', users)
+	const { data: messagesCount } = useQuery(
+		['allMessagesCount'],
+		() => axios.get('/api/get_all_messages_count').then((res) => res.data),
+		{
+			refetchInterval: 8000
+		}
+	)
 	const stats = [
 		{
 			id: 1,
@@ -115,6 +121,13 @@ export default function About({
 		{ id: 2, name: 'Total Posts', value: <CountUp end={posts?.length} /> },
 		{ id: 3, name: ' Total Users', value: <CountUp end={users?.length} /> }
 	]
+	const stats2 = [
+		{
+			id: 4,
+			name: 'Total Messages (encrypted)',
+			value: <CountUp end={messagesCount} />
+		}
+	]
 
 	return (
 		<div className='bg-white'>
@@ -122,6 +135,25 @@ export default function About({
 				<div className='mx-auto max-w-7xl px-6 lg:px-8'>
 					<dl className='grid grid-cols-1 gap-y-16 gap-x-8 text-center lg:grid-cols-3'>
 						{stats.map((stat) => (
+							<div
+								key={stat.id}
+								className='mx-auto flex max-w-xs flex-col gap-y-4'>
+								<dt className='text-base leading-7 text-gray-600'>
+									{stat.name}
+								</dt>
+								<dd className='order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>
+									{stat.value}
+								</dd>
+							</div>
+						))}
+					</dl>
+				</div>
+			</div>
+
+			<div className='bg-white pt-12 pb-12'>
+				<div className='mx-auto max-w-7xl px-6 lg:px-8'>
+					<dl className='grid grid-cols-1 gap-y-16 gap-x-8 text-center'>
+						{stats2.map((stat) => (
 							<div
 								key={stat.id}
 								className='mx-auto flex max-w-xs flex-col gap-y-4'>
