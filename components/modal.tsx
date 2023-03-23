0f16126fafd01d402b26f6de283ec45a32fbf0e9
Jaredd <jaredd@Jared-Dahlkes-Macbook.local>
2023-03-22 17:02:12 -0700

    improve login page style

--- components/modal.tsx
+++ components/modal.tsx
@@ -35,8 +35,20 @@ export default function Modal({
 				<button
 					onClick={() => setOpen(false)}
 					//absolute right-4 top-4 z-20
-					className='btn btn-sm btn-circle absolute right-4 top-4 text-primary bg-primary-content'>
-					<XMarkIcon />
+					className='btn btn-sm btn-circle btn-outline absolute right-2 top-2'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						className='h-6 w-6'
+						fill='none'
+						viewBox='0 0 24 24'
+						stroke='currentColor'>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							strokeWidth='2'
+							d='M6 18L18 6M6 6l12 12'
+						/>
+					</svg>
 				</button>
 
 				<section className='py-6'>
